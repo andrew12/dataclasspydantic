f56dd15fe651faefb9e6e2046cf910a3e72a68f8
Andrew Herbig <notandrewherbig@gmail.com>
2020-12-07 18:46:18 -0500

    first commit

---
+++ test.py
@@ -0,0 +1,18 @@
+from dataclasses import dataclass
+from typing import Literal
+
+from pydantic import BaseModel
+
+Sum = Literal[1, 2]
+
+
+@dataclass
+class Base:
+    literal: Sum
+
+
+class What(BaseModel):
+    base: Base
+
+
+What(base=Base(literal=1))
